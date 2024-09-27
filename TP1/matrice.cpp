061784f72d9770cd6f45e2dcbd0fa74d9b46d85b
Kenji on Windows <kenji.chikhaoui@gmail.com>
2024-09-28 00:45:03 +0200

    windows update

--- TP1/matrice.cpp
+++ TP1/matrice.cpp
@@ -1,9 +1,10 @@
 #include "matrice.h"
 #include <stdio.h>
-#include <unistd.h>
+
 #include "functions.h"
 
 #if defined(__linux__)
+    #include <unistd.h>
     #include <sys/ioctl.h>
 #elif defined(_WIN32)
     #include <windows.h>
@@ -13,6 +14,7 @@
 char Matrice::init(int a, int b){
     L = a;
     C = b;
+    tab = new double[L*C];
     for(int i=0;i<L*C;i++){
         tab[i]=0;
     }
@@ -28,18 +30,22 @@ char Matrice::affiche(){
         window_height = w.ws_row;
     }
     #elif defined(_WIN32)
-        CONSOLE_SCREEN_BUFFER_INFO csbi;
-        int columns, rows;
+      CONSOLE_SCREEN_BUFFER_INFO csbi;
+      int columns, rows;
+      columns=0;
+      rows=0;
 
         // Get the console screen buffer info
-        if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
-            columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-            rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-        }
-        window_length = columns;
-        window_height = rows;
+      if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
+          columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+          rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+      };
+      window_length = columns;
+      window_height = rows;
     #else
         printf("You're not on Linux or Windows, there is no guarantee for the display of the matrix");
+        window_length=0;
+        window_height=0;
     #endif
 
     int element_size=1;
@@ -49,7 +55,7 @@ char Matrice::affiche(){
             element_size = count_double_size(tab[k]);
         }
     };
-    if ((element_size+3)*C>window_length-5 || window_height < L+4){
+    if ((element_size+3)*C>(window_length-5) || window_height < (L+4)){
         printf("the matrix may not display properly \n");
     }
     
