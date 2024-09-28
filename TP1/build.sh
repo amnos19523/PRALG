016172945041dc82bc81f754e742417ee0b89312
Kenji on Windows <kenji.chikhaoui@gmail.com>
2024-09-28 14:02:49 +0200

    build update

--- TP1/build.sh
+++ TP1/build.sh
@@ -1,8 +1,8 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then 
 	mkdir Build
-        cd Build
-        cmake ..
-        cmake --build .
+    cd Build
+    cmake ..
+    cmake --build .
 	echo $OSTYPE
 else
 	mkdir Build
